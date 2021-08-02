207b50cb9d4bc818fdd9ec107d742af0ca4a04a3
wowls1227 <53048187+wowls1227@users.noreply.github.com>
2021-08-02 14:15:57 +0900

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -13,7 +13,7 @@ terraform {
   required_version = ">= 0.15"
 
   backend "remote" {
-    organization = "<YOUR_TERRAFORM_ORG>"
+    organization = "http://terraform.io/hashicorp_learn-self"
 
     workspaces {
       name = "sentinel-example"
